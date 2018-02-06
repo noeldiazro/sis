cc3ff07b692d0548610532aaf254fc14e352e6de
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-02-06 11:22:42 +0100

    Test suites reorganized

--- source/sis/report/AllTests.java
+++ source/sis/report/AllTests.java
@@ -1,12 +1,11 @@
-package sis;
+package sis.report;
 
 import junit.framework.TestSuite;
 
 public class AllTests {
     public static TestSuite suite() {
 	TestSuite suite = new TestSuite();
-	suite.addTest(sis.report.AllTests.suite());
-	suite.addTest(sis.studentinfo.AllTests.suite());
+	suite.addTestSuite(RosterReporterTest.class);
 	return suite;
     }
 }
