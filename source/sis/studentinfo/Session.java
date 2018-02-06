0133c303d435ff198294626cf947b31c43f784d2
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-02-06 10:58:32 +0100

    Session public methods atop

--- source/sis/studentinfo/Session.java
+++ source/sis/studentinfo/Session.java
@@ -15,10 +15,21 @@ public class Session implements Iterable<Student> {
     private List<Student> students = new ArrayList<Student>();
     private Date startDate;
 
-
     public Session(Course course, Date startDate) {
 	this.course = course;
 	this.startDate = startDate;
+    }
+
+    public int getNumberOfStudents() {
+	return students.size();
+    }
+
+    public void enroll(Student student) {
+	students.add(student);
+    }
+
+    public Iterator<Student> iterator() {
+	return students.iterator();
     }
 
     String getDepartment() {
@@ -29,14 +40,6 @@ public class Session implements Iterable<Student> {
 	return course.getNumber();
     }
     
-    public int getNumberOfStudents() {
-	return students.size();
-    }
-
-    public void enroll(Student student) {
-	students.add(student);
-    }
-
     Student get(int index) {
 	return students.get(index);
     }
@@ -55,9 +58,5 @@ public class Session implements Iterable<Student> {
 	    courseDurationInWeeks * daysPerWeek - daysFromFridayToMonday;
 	calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
       	return calendar.getTime();
-    }
-
-    public Iterator<Student> iterator() {
-	return students.iterator();
     }
 }
