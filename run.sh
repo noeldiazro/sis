e57be3f4c22940dbc4c8e139500babb88b2e2877
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-02-06 13:25:51 +0100

    Migrated to Maven

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-javac -classpath junit-3.8.2.jar -Xlint:deprecation -d classes source/sis/studentinfo/*.java source/sis/report/*.java source/sis/AllTests.java
-
-if [ $? -eq 0 ]; then
-    java -cp classes:junit-3.8.2.jar junit.textui.TestRunner sis.AllTests
-fi
