2784803ae0b00efceb65000dd42ac266bf9215b8
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-03-13 17:18:10 +0100

    Synchronized queue in server

--- src/main/java/sis/search/Server.java
+++ src/main/java/sis/search/Server.java
@@ -1,10 +1,12 @@
 package sis.search;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 class Server extends Thread {
-    private List<Search> queue = new LinkedList<Search>(); // flaw!
+    private List<Search> queue =
+	Collections.synchronizedList(new LinkedList<Search>());
     private ResultsListener listener;
     
     Server(ResultsListener listener) {
